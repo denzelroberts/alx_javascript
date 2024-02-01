c86e4547ede646345ae3ad436ee77b48dd6db706
denzelroberts <iamdenzelroberts@gmail.com>
2024-02-01 13:42:05 +0000

    comment

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 class Rectangle {
     constructor (w,h){
         if(w > 0 && h > 0){
@@ -5,7 +6,7 @@ class Rectangle {
             this.height = h;
         }
         else{
-            
+
         }
     }
 }
